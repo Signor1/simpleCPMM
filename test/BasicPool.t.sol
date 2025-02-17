93679374d002cfbff2ec967023fc88234b0a0d29
signor1 <emmanuelomemgboji@gmail.com>
2025-02-17 23:36:32 +0100

    chore: writing test

--- test/BasicPool.t.sol
+++ test/BasicPool.t.sol
@@ -1,6 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {Test} from "forge-std/Test.sol";
+import {console} from "forge-std/console.sol";
 import {BasicPool} from "../src/BasicPool.sol";
+import {TokenA} from "../src/TokenA.sol";
+import {TokenB} from "../src/TokenB.sol";
 
-contract BasicPoolTest is BasicPool {}
+contract BasicPoolTest is Test {
+    BasicPool pool;
+    TokenA tokenA;
+    TokenB tokenB;
+
+    // LPs address
+    address[] public lps = [address(0x1), address(0x2), address(0x3)];
+    // swappers address
+    address[] public swappers = [
+        address(0x4),
+        address(0x5),
+        address(0x6),
+        address(0x7),
+        address(0x8),
+        address(0x9)
+    ];
+
+    function setUp() public {
+        tokenA = new TokenA(address(this));
+        tokenB = new TokenB(address(this));
+        pool = new BasicPool();
+    }
+}
