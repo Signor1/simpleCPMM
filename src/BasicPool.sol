e18747a712f61e8caa94759c6c22d803f471d5a1
signor1 <emmanuelomemgboji@gmail.com>
2025-02-17 15:50:18 +0100

    feat: added method for liquidity

--- src/BasicPool.sol
+++ src/BasicPool.sol
@@ -59,4 +59,46 @@ contract BasicPool is Ownable, ERC20 {
         require(_tokenB != address(0), "Invalid address");
         tokenB = IERC20(_tokenB);
     }
+
+    // Add liquidity to the pool
+    function addLiquidity(uint256 amountA, uint256 amountB) external {
+        require(amountA > 0 && amountB > 0, "Amounts must be > 0");
+
+        // Transfer tokens from user to contract
+        tokenA.transferFrom(msg.sender, address(this), amountA);
+        tokenB.transferFrom(msg.sender, address(this), amountB);
+
+        // Calculate LP tokens to mint (proportional to liquidity added)
+        uint256 liquidity;
+        if (pooltotalSupply == 0) {
+            liquidity = sqrt(amountA * amountB); // Initial liquidity
+        } else {
+            liquidity = min(
+                (amountA * pooltotalSupply) / reservoirA,
+                (amountB * pooltotalSupply) / reservoirB
+            );
+        }
+
+        // Update reserves and LP tokens
+        reservoirA += amountA;
+        reservoirB += amountB;
+        lpbalanceOf[msg.sender] += liquidity;
+        pooltotalSupply += liquidity;
+
+        emit LiquidityAdded(msg.sender, amountA, amountB, liquidity);
+    }
+
+    // Helper functions
+    function sqrt(uint256 x) internal pure returns (uint256 y) {
+        uint256 z = (x + 1) / 2;
+        y = x;
+        while (z < y) {
+            y = z;
+            z = (x / z + z) / 2;
+        }
+    }
+
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        return a < b ? a : b;
+    }
 }
